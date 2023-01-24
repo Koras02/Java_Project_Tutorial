2ac570b4bbe155c7abb364e5797551b81d075172
Koras02 <wjdgh0727@gmail.com>
2023-01-24 20:50:12 +0900

    Java 1강 - 변수와 자료형

--- src/variables/Animal.java
+++ src/variables/Animal.java
@@ -3,7 +3,7 @@ package variables;
 public class Animal {
     public static void main(String[] args) {
         String Animal;
-        Animal = "cat";
+        Animal = "cat";//
 
         System.out.println(Animal);
     }
